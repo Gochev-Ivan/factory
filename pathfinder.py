559de3da648166556c752f4ccf1aae0588d277c5
Gochev-Ivan <31308329+Gochev-Ivan@users.noreply.github.com>
2017-11-04 20:12:31 +0100

    Add files via upload

--- pathfinder.py
+++ pathfinder.py
@@ -30,7 +30,7 @@ for i in range(factory_length):
     y_coord = 0
     for j in range(factory_width):
         environment[i][j] = (int(x_coord / 0.5), int(y_coord / 0.5))
-        y_coord -= coord_step
+        y_coord += coord_step
     x_coord += coord_step
 
 for i in range(factory_length):
